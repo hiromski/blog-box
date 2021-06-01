db48ce40a761974c0668bf32f777f12e6f684cbf
hiromski <hiromi3310@gmail.com>
2021-06-01 17:55:13 -0500

    Updates

---
+++ static/js/main.69f4f5ae.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpblogbox=this.webpackJsonpblogbox||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(0));var b=function(){return Object(s.jsx)("div",{children:"homepage"})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(b,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.69f4f5ae.chunk.js.map
